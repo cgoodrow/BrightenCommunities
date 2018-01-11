e157c4302325456cc9bacc3fa55c8e4b7423edf3
Chris Goodrow <goodrow.chris4@gmail.com>
2018-01-10 22:28:57 -0500

    added blog and created partial view for blog create.

---
+++ BrightenCommunities/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BrightenCommunities.MvcApplication" Language="C#" %>
---
+++ BrightenCommunities/Models/Blogs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace BrightenCommunities.Models
+{
+    public class Blogs
+    {   
+        [Key]
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Post { get; set; }
+    }    
+}
