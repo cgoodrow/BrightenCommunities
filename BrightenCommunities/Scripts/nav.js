362d1a9cab9c43fc685d0676b06f9a11432cb574
Chris Goodrow <goodrow.chris4@gmail.com>
2018-01-23 21:58:44 -0500

    off menu nav in progress

---
+++ BrightenCommunities/Scripts/nav.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+
+    console.log(isActive);
+    var isActive = false;
+
+    $('.js-menu').on('click', function () {
+        console.log("click");
+        if (isActive) {
+            $(this).removeClass('active');
+            $('body').removeClass('menu-open');
+        } else {
+            $(this).addClass('active');
+            $('body').addClass('menu-open');
+        }
+        isActive = !isActive;
+    });
+});
